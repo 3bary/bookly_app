6dbf07f70373cc775091839498e27617b17dc3e0
Mohamed Ashraf <medoalex350@gmail.com>
2023-03-03 00:01:10 +0200

    add book details view

---
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/book_details_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsBody(
+
+      ),
+    );
+  }
+}
