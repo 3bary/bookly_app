bb13c802c2cb7fca3ab9f0c30b77fe5628648dd1
Mohamed Ashraf <medoalex350@gmail.com>
2023-03-26 04:19:53 +0200

    newest books cubit

---
+++ lib/features/home/presentation/view_model/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../../data/models/book_model/BookModel.dart';
+import '../../../data/repos/home_repo.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) => emit(NewestBooksFailure(failure.errorMessage)),
+      (books) => emit(NewestBooksSuccess(books)),
+    );
+  }
+}
