eff4c9399c5e7d80dbee0c17caf1b94c94731d60
Mohamed Ashraf <medoalex350@gmail.com>
2023-04-06 05:31:16 +0200

    cached network image

--- lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,20 +1,22 @@
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomBookItem extends StatelessWidget {
   const CustomBookItem({Key? key, required this.image}) : super(key: key);
   final String image;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6/4,
-      child: Container(
-        decoration:  BoxDecoration(
-          borderRadius: BorderRadius.circular(14.0),
-            image: DecorationImage(
-                image: NetworkImage(image),
-                fit: BoxFit.fill,
-            ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6/4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+            imageUrl: image,
+          errorWidget: (context, url, error) => const Icon(FontAwesomeIcons.circleExclamation),
+          placeholder: (context, url,) => const Center(child: CircularProgressIndicator(),),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/features/home/presentation/view_model/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/view_model/newest_books_cubit/newest_books_cubit.dart';
+import 'package:bookly_app/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObserver();
   setupServiceLocator();
   runApp(const BooklyApp());
 }
